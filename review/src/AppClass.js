4a23b0d4e80af5977f71a7dd6f6ee1832b351d63
Kat <kyevsukov@gmail.com>
2021-09-28 12:46:42 -0400

    finished appclass and app func

--- review/src/AppClass.js
+++ review/src/AppClass.js
@@ -1,22 +1,32 @@
 import React from "react";
 
 class AppClass extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "Warren"
-    };
+  state = {
+    name: "Allison",
+    age: 33
+  }
+
+  componentDidMount() {
+    console.log("AppClass: Component Mounts");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("AppClass: Component Updates");
+    console.log("old props: ", prevProps);
+    console.log("new props: ", this.props);
   }
 
   handleNameButtonClick = (e) => {
+    console.log("-----------");
+    console.log("AppFunc: Changes State");
     this.setState({
       ...this.state,
-      name: "Allison"
+      name: "Chris"
     });
   };
 
-  
   render() {
+    console.log("AppFunc: Renders DOM");
     return (
       <div>
         <h1>Hello {this.state.name}.</h1>
